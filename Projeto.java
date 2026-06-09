762f1247fe9d9849f401b69e14ee5d9a4ce4416c
Henrique <henriquelopis066@gmail.com>
2026-06-09 20:22:16 -0300

    Main pra projeto

--- Projeto.java
+++ Projeto.java
@@ -2,7 +2,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class Projeto {
 
     // =========================
     // CLASSE LIVRO
@@ -157,9 +157,7 @@ public class Main {
         }
     }
 
-    // =========================
-    // MAIN
-    // =========================
+  
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
